3e038a685a2b274ec30becc708ff8db767f9892c
LuisaDev <ingluisago@gmail.com>
2025-07-14 09:06:18 -0500

    Incluir DataMember

--- Entidades/BUsuarios.cs
+++ Entidades/BUsuarios.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
 namespace Entidades
 {
+    [DataContract]
+    [Serializable]
     public class BUsuarios
     {
+        [DataMember]
         public int Id { get; set; }
+        [DataMember]
         public string Nombre { get; set; }
+        [DataMember]
         public DateTime Fecha { get; set; }
+        [DataMember]
         public char Sexo { get; set; }
     }
 }
